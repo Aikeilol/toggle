386606ba05b9bbb76d760d95b30d305c9f325542
Aikeilol <garena3307@mail.ru>
2022-01-11 21:33:32 +0300

    toggle

--- script.js
+++ script.js
@@ -1,30 +1,12 @@
 "use strict";
-const btns = document.querySelectorAll('button'),
-      wrapper = document.querySelector('.btn-block');
-// console.log(btns[0].classList.length);
-// console.log(btns[0].classList.item(0));
-// console.log(btns[2].classList.add("red"));
-// // console.log(btns[0].classList.remove("blue"));
-// // console.log(btns[0].classList.toggle("blue"));
+const btns = document.querySelectorAll('button');
+const wrapper = document.querySelector('.btn-block');
 
-// if (btns[2].classList.contains("red")){
-//     console.log("red");
-// }
-
-// btns[0].addEventListener('click', (e) =>{
-// if (!btns[1].classList.contains("red")){
-//     btns[1].classList.add("red");
-// }else{
-//     btns[1].classList.remove("red");
-// }
-// btns[1].classList.toggle("blue");
-// });
-
-btns[0].addEventListener("click", (e) => {
 btns.forEach(item => {
-item.classList.toggle("blue");
-});
-});
+    item.addEventListener('click', () => {
+        item.classList.toggle("blue");
+    })
+})
 
 wrapper.addEventListener('click', (e) => {
 if(e.target && e.target.classList.contains("blue")){
